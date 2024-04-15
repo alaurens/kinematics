2e29e243d52d8c158591f830d5b939483cb94975
alaurens <laurens_antoine@hotmail.fr>
2024-04-16 01:51:45 +0200

    Finish main.

--- main.cc
+++ main.cc
@@ -1,15 +1,27 @@
-
-#include <cmath>
 #include <iostream>
+#include <cmath>
 #include <string>
-#include <Eigen/Dense>
-#include "robot.h"
+#include "kinematics.h"
+#include "three_joint_robot.h"
+#include "absl/strings/str_join.h"
 #include <memory>
 #include <utility>
 #include <array>
-// #include trans
- 
+  
 
 int main(int argc, char* argv[]){
+
+  std::array<double, 4> link_lengths{1.0, 1.0, 1.0, 1.0};
+  std::unique_ptr<alaurens::ThreeJointRobot> robot( 
+      std::make_unique<alaurens::ThreeJointRobot>("robot", link_lengths)
+  );
+  robot->UpdateRobotState({M_PI / 2, 0.0, 0.0});
+  std::array<double, 16> result;
+  alaurens::ForwardKinematics(*robot, absl::MakeSpan(result));
+  for (int i=0; i < 4; i++){
+   std::cout 
+       <<  absl::StrJoin(absl::MakeSpan(result).subspan(i*4, 4), ", ")
+       << std::endl; 
+  }
   return 0;
 }
